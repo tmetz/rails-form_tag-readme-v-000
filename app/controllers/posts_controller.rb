6d2e7599d82d0507845bd69db5b12bbf0d6214f7
Tammy Metz <tammy@tammymetz.com>
2018-12-28 16:18:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,9 @@ class PostsController < ApplicationController
 
   end
 
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
